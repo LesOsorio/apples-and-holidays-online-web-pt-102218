e989302b459cb0c91f058ad4db11628885b86a63
Leslie Osorio <lesosorio17@gmail.com>
2018-11-16 00:15:40 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,8 +49,7 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   holiday_hash[:winter][:new_years] << "Lights"
   holiday_hash[:winter].each do |supply|
-    supply.any?
-    holiday_hash
+    supply.all? 
   end
 end
 
