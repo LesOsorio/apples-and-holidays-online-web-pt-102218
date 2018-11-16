6bd3a278e658615c1116761eecfc4b53428d4a07
Leslie Osorio <lesosorio17@gmail.com>
2018-11-16 01:02:06 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,10 +63,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    puts season.capitalize
+    puts "#{season.capitalize}"
     holiday_hash.each do |holiday, supplies|
-      puts holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}
-      supplies.join(",")
+      puts "#{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")} #{supplies.join(",")
     end
   end
 end
@@ -82,23 +81,3 @@ def all_holidays_with_bbq(holiday_hash)
   end.flatten.compact
 end
 
-
-
-
-
-
-
-
-
-
-puts "#{season.capitalize}"
-holiday_hash.each do |holiday, supplies|
-  puts "#{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")} #{supplies.join(",")}
-
-
-
-
-  holiday_hash.each do |season, holiday|
-    puts "#{season.capitalize}:"
-    holiday.each do |holiday, supplies|
-    puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
